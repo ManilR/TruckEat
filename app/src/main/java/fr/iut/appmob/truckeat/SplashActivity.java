93e07cf2cfaf3ed16103d9d0cb8454c764a90223
Greg-Orius <73349066+Greg-Orius@users.noreply.github.com>
2021-03-04 19:54:54 +0100

    Implémentation des visu

--- app/src/main/java/fr/iut/appmob/truckeat/SplashActivity.java
+++ app/src/main/java/fr/iut/appmob/truckeat/SplashActivity.java
@@ -5,15 +5,44 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 //Splash Screen with the app's logo
 
 public class SplashActivity extends AppCompatActivity {
+
+    Handler handler;
+    Animation rotateAnimation;
+    ImageView imageView;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Intent intent = new Intent(this, MainActivity.class);
-        startActivity(intent);
-        finish();
+        setContentView(R.layout.activity_splash_screen);
+
+        handler = new Handler();
+        handler.postDelayed(new Runnable(){
+            @Override
+            public void run(){
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                imageView = (ImageView)findViewById(R.id.imageRoue);
+
+                rotateAnimation();
+                startActivity(intent);
+                finish();
+            }
+        },3000);
+
+
+
+    }
+
+    private void rotateAnimation(){
+
+        rotateAnimation= AnimationUtils.loadAnimation(this,R.anim.rotate);
+        imageView.startAnimation(rotateAnimation);
     }
 }
